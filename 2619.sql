3f767d69197bb8255a3c2466d9b509d3e0c1e01c
Leo <43527720+lefredoh@users.noreply.github.com>
2023-03-03 00:01:28 -0300

    Update 2619.sql

--- 2619.sql
+++ 2619.sql
@@ -1,1 +1,7 @@
-SELECT d.name, v.name, d.price FROM products as d INNER JOIN providers AS v ON d.id_providers = v.id INNER JOIN categories AS cat ON d.id_categories = cat.id WHERE cat.name = 'Super Luxury' AND d.price > 1000;
+SELECT d.name, v.name, d.price 
+FROM products as d 
+INNER JOIN providers AS v 
+ON d.id_providers = v.id 
+INNER JOIN categories AS cat 
+ON d.id_categories = cat.id 
+WHERE cat.name = 'Super Luxury' AND d.price > 1000;
